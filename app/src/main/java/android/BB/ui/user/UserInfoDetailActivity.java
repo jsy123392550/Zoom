b2aeb54d487d383408cc70808da6e355387960b9
KalinaRain <327509740ljk@gmail.com>
2015-08-27 11:46:16 +0800

    perfect the userinfo

--- app/src/main/java/android/BB/ui/user/UserInfoDetailActivity.java
+++ app/src/main/java/android/BB/ui/user/UserInfoDetailActivity.java
@@ -4,7 +4,6 @@ import android.BB.bean.nearby.UserInfo;
 import android.BB.finals.MyConstants;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -23,7 +22,7 @@ public class UserInfoDetailActivity extends AppCompatActivity implements View.On
     private Toolbar toolbar;
     private TextView tv_toolbar_center;
     private RelativeLayout rl_portrait;
-    private RelativeLayout rl_username;
+    private RelativeLayout rl_nickname;
     private RelativeLayout rl_bbnumber;
     private RelativeLayout rl_gender;
     private RelativeLayout rl_district;
@@ -41,14 +40,14 @@ public class UserInfoDetailActivity extends AppCompatActivity implements View.On
         toolbar = (Toolbar) findViewById(R.id.userinfo_detail_toolbar);
         tv_toolbar_center = (TextView) toolbar.findViewById(R.id.toolbar_tv);
         rl_portrait = (RelativeLayout) findViewById(R.id.rl_portrait);
-        rl_username = (RelativeLayout) findViewById(R.id.rl_username);
+        rl_nickname = (RelativeLayout) findViewById(R.id.rl_nickname);
         rl_bbnumber = (RelativeLayout) findViewById(R.id.rl_bbnumber);
         rl_gender = (RelativeLayout) findViewById(R.id.rl_gender);
         rl_district = (RelativeLayout) findViewById(R.id.rl_district);
         rl_personality_signature = (RelativeLayout) findViewById(R.id.rl_personality_signature);
         ratingBar_credit = (RatingBar) findViewById(R.id.ratingbar_credit);
         rl_portrait.setOnClickListener(this);
-        rl_username.setOnClickListener(this);
+        rl_nickname.setOnClickListener(this);
         rl_bbnumber.setOnClickListener(this);
         rl_gender.setOnClickListener(this);
         rl_district.setOnClickListener(this);
@@ -66,19 +65,24 @@ public class UserInfoDetailActivity extends AppCompatActivity implements View.On
     public void onClick(View v) {
         Intent intent = null;
         switch (v.getId()) {
-            case R.id.rl_portrait:
-                intent = new Intent(UserInfoDetailActivity.this, UserInfoDetailActivity.class);
+            case R.id.rl_nickname:
+                intent = new Intent(UserInfoDetailActivity.this, UserInfoNickNameChanging.class);
                 break;
-            case R.id.ll_user_BB:
-                intent = new Intent(UserInfoDetailActivity.this, UserBBhistoryActivity.class);
+            case R.id.rl_bbnumber:
+                intent = new Intent(UserInfoDetailActivity.this, UserInfoBBNumberChanging.class);
                 break;
-            case R.id.ll_user_BB_coin:
+            case R.id.rl_gender:
+                /*开启一个对话框*/
                 intent = new Intent(UserInfoDetailActivity.this, UserBBcoinActivity.class);
                 break;
-            case R.id.ll_user_setting:
-                intent = new Intent(UserInfoDetailActivity.this, UserBBSettingActivity.class);
+            case R.id.rl_district:
+                intent = new Intent(UserInfoDetailActivity.this, UserInfoDistrictChanging.class);
+                break;
+            case R.id.rl_personality_signature:
+                intent = new Intent(UserInfoDetailActivity.this, UserInfoPersonalSignatureChanging.class);
                 break;
             default:
+                intent = new Intent();
                 break;
         }
         startActivity(intent);
